d169d25acf2143ab920a284a0fffde5e1ae777b3
Marcelo <marcelotoromontoya@gmail.com>
2023-06-14 15:02:28 -0400

    codigo refactorizado, definitivo

--- class_persona.rb
+++ class_persona.rb
@@ -1,51 +1,70 @@
 class Person
 
-    attr_accessor :first_name, :last_name, :age, :type
+    attr_accessor :first_name, :last_name
 
-    def initialize(first_name, last_name, age, type)
+    def initialize(first_name, last_name)
         @first_name = first_name
         @last_name = last_name
-        # @age = age
-        # @type = type
     end
 
-    def birthday
-        @age += 1
-    end
 end
 
 class Student < Person
 
-    def talk
-        puts "¿Aquí es la clase de programación con Ruby?"
+    def initialize
+        super(first_name, last_name)
+    end
+
+    def introduce(first_name, last_name)
+        "¡Hola, profesor! Mi nombre es #{first_name} #{last_name}"        
     end
     
-    def introduce
-        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."        
+    def talk
+        "¿Aquí es la clase de programación con Ruby?"
     end
 
 end
 
 class Teacher  < Person
 
-    def talk
-        puts "Bienvenidos a la clase de programación con Ruby!"        
+    def initialize
+        #Los constructores son independientes. Se invocan a la clase hija con la palabra reservada "super".
+        #"super" debe estar en la primera línes del inicialize, después genera error. "super" es equivalente
+        # a copiar el códifo de la clase padre. 
+        super(first_name, last_name)
     end
 
-    def introduce
-        puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."        
+    def introduce(first_name, last_name)
+         "¡Hola, alumnos! Mi nombre es #{first_name} #{last_name}"        
+    end
+
+    def talk
+        "Bienvenidos a la clase de programación con Ruby!"        
     end
     
 end
 
 class Parent  < Person
 
-    def talk
-        puts "¿Aquí es la reunión de apoderados?"        
+    def initialize
+        super(first_name, last_name)
     end
 
-    def introduce
-        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."        
+    def introduce(first_name, last_name)
+        "Hola, soy uno de los apoderados. Mi nombre es #{first_name} #{last_name}"        
+    end
+
+    def talk
+        "¿Aquí es la reunión de apoderados?"        
     end
 
 end
+
+alumno_1 = Student.new
+puts "#{alumno_1.talk} #{alumno_1.introduce("Juanin", "Juan Harry")}."
+puts
+profesor_1 = Teacher.new
+puts "#{profesor_1.introduce("Juan Carlos", "Bodoque")}. #{profesor_1.talk}"
+puts
+padre_1 = Parent.new
+puts "#{padre_1.introduce("Tulio", "Triviño")}. #{padre_1.talk}"
